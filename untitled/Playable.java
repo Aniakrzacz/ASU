901cadfad8d02289943b280a169174e8762abdd4
Aniakrzacz <anna.krzaczkowska@student.uj.edu.pl>
2024-05-10 14:27:48 +0200

    zmiana struktury

--- untitled/Playable.java
+++
@@ -1,4 +0,0 @@
-public interface Playable {
-    //komentarz do usuniecia w przyszlosci, numer i symbol to po prostu opis karty aktualnie na górze.
-    public boolean isPlayable(int number, int symbol)
-}
