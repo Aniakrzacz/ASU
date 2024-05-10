22bb69c9d226860f22213b0bd06cfe98d478109d
Szymon Trofimiec <sztrofimiec@gmail.com>
2024-05-10 13:55:22 +0200

    .

---
+++ untitled/Playable.java
@@ -0,0 +1,4 @@
+public interface Playable {
+    //komentarz do usuniecia w przyszlosci, numer i symbol to po prostu opis karty aktualnie na górze.
+    public boolean isPlayable(int number, int symbol)
+}
